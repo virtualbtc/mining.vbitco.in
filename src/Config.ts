664c6c6c6db64bbcff3090e157e32416f1b1995a
Hanul <hanul@hanul.me>
2021-05-23 02:30:50 +0900

    update design

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
 export default class Config {
     public static PROVIDER_URL = "wss://kovan.infura.io/ws/v3/96ce12f4a29c44699587732ebf110b75";
-    public static VBTC_ADDRESS = "0xAC5b9a422c6f71DD2ac73c7Fa7A7896136e6E709";
+    public static VBTC_ADDRESS = "0xfe6D468bB4DD530E0f5eE98b58e37e11DaAAaF31";
 }
