3bcff86420eb19bb09aabc55d99f7d3917dc3c1c
Hanul <hanul@hanul.me>
2021-07-16 11:16:10 +0900

    mainnet config

--- src/Config.ts
+++ src/Config.ts
@@ -6,15 +6,14 @@ export default {
     infuraId: INFURA_ID,
 
     // Kovan
-    rpc: `https://kovan.infura.io/v3/${INFURA_ID}`,
-
+    //rpc: `https://kovan.infura.io/v3/${INFURA_ID}`,
     // Mainnet
-    //rpc: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+    rpc: `https://mainnet.infura.io/v3/${INFURA_ID}`,
 
     contracts: {
         // Kovan
-        VirtualBitcoin: "0xfe6D468bB4DD530E0f5eE98b58e37e11DaAAaF31",
+        //VirtualBitcoin: "0xfe6D468bB4DD530E0f5eE98b58e37e11DaAAaF31",
         // Mainnet
-        //VirtualBitcoin: "0x84e7ae4897b3847b67f212aff78bfbc5f700aa40",
+        VirtualBitcoin: "0x84e7ae4897b3847b67f212aff78bfbc5f700aa40",
     },
 };
