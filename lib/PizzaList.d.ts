9cf0b2be4bca914e7c4e94183e57ecaee90119e0
Hanul <hanul@hanul.me>
2021-07-18 15:56:14 +0900

    bug fix

--- lib/PizzaList.d.ts
+++ lib/PizzaList.d.ts
@@ -1,6 +1,5 @@
 import { DomNode } from "@hanul/skynode";
 export default class PizzaList extends DomNode {
-    private onlyOwned;
     constructor();
     private loadPizzaList;
 }
--- src/PizzaList.ts
+++ src/PizzaList.ts
@@ -2,12 +2,9 @@ import { BigNumber } from "@ethersproject/bignumber";
 import { DomNode } from "@hanul/skynode";
 import { ethers } from "ethers";
 import VirtualBitcoinContract from "./contracts/VirtualBitcoinContract";
-import Wallet from "./ethereum/Wallet";
 import Pizza from "./Pizza";
 
 export default class PizzaList extends DomNode {
-
-    private onlyOwned = false;
 
     constructor() {
         super(".pizza-list");
@@ -17,23 +14,16 @@ export default class PizzaList extends DomNode {
     private async loadPizzaList() {
         this.empty().appendText("Loading...");
 
-        const owner = await Wallet.loadAddress();
-        if (owner !== undefined) {
+        const pizzaCount = (await VirtualBitcoinContract.getPizzaCount()).toNumber();
 
-            const pizzaCount = (await VirtualBitcoinContract.getPizzaCount()).toNumber();
-
-            this.empty();
-            for (let pizzaId = pizzaCount - 1; pizzaId >= 0; pizzaId -= 1) {
-                (async () => {
-                    const pizzaData = await VirtualBitcoinContract.getPizza(BigNumber.from(pizzaId));
-                    if (
-                        (this.onlyOwned !== true || pizzaData.owner === owner) &&
-                        pizzaData.owner !== ethers.constants.AddressZero
-                    ) {
-                        new Pizza(pizzaId, pizzaData).appendTo(this);
-                    }
-                })();
-            }
+        this.empty();
+        for (let pizzaId = pizzaCount - 1; pizzaId >= 0; pizzaId -= 1) {
+            (async () => {
+                const pizzaData = await VirtualBitcoinContract.getPizza(BigNumber.from(pizzaId));
+                if (pizzaData.owner !== ethers.constants.AddressZero) {
+                    new Pizza(pizzaId, pizzaData).appendTo(this);
+                }
+            })();
         }
     }
 }
